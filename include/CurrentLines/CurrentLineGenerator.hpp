7012c002fa7d5593b605f95e334e44fb81ba1cac
GuVictory <gubanova.vi10@gmail.com>
2020-08-17 18:05:46 +0300

    Add CMake corfig for main lib, doc and tests

---
+++ include/CurrentLines/CurrentLineGenerator.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Виктория Губанова on 17.08.2020.
+//
+
+#ifndef CURRENTLINES_CURRENTLINEGENERATOR_HPP
+#define CURRENTLINES_CURRENTLINEGENERATOR_HPP
+
+
+class CurrentLineGenerator {
+
+};
+
+
+#endif //CURRENTLINES_CURRENTLINEGENERATOR_HPP
---
+++ src/CurrentLines/CurrentLineGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Виктория Губанова on 17.08.2020.
+//
+
+#include <CurrentLines/CurrentLineGenerator.hpp>
